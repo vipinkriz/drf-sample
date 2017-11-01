8c265ae8fccecae9efc34ba02c776bbf8b340a14
Vipin <kriz.vipin@gmail.com>
2017-11-01 09:43:39 +0530

    serializers working wiht successfull results curl -g localhost:8000/chains/ curl -g localhost:8000/stores/ curl -g localhost:8000/employees/
    above links works fine

--- server/urls.py
+++ server/urls.py
@@ -19,3 +19,13 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+from rest_framework.routers import DefaultRouter
+from retail.views import ChainViewSet, StoreViewSet, EmployeeViewSet
+
+router = DefaultRouter()
+router.register(prefix='chains', viewset=ChainViewSet)
+router.register(prefix='stores', viewset=StoreViewSet)
+router.register(prefix='employees', viewset=EmployeeViewSet)
+
+urlpatterns = router.urls
